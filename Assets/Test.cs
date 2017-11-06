ed0b345f1ecebcb3ae98284fe25c6a673f817865
ユニティクラス <wolf5052tokyo@yahoo.com>
2017-11-05 19:52:27 -0800

    ScriptTest プロジェクト課題

---
+++ Assets/Test.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour{
+
+	void Start(){
+
+		int[] points = new int[5];
+
+		points [0] = 10;
+		points [1] = 20;
+		points [2] = 30;
+		points [3] = 40;
+		points [4] = 50;
+
+		for (int i = 4; i >= 0; i--) 
+		{
+			Debug.Log (points [i]);
+		}
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	}
